3b502da21209f0d8be267469babf1ae1cc58f548
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-01-22 07:30:17 +0700

    hotfix/refractor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="row">
-
     <InsertName />
-
-    <!-- <Player1/>
-
-    <Roll/>
-
-    <Player2/> -->
-
   </div>
 </template>
 
@@ -16,20 +8,11 @@
 // @ is an alias to /src
 
 import InsertName from '../components/InsertName'
-// import Player1 from '../components/Player1'
-// import Roll from '../components/Roll'
-// import Player2 from '../components/Player2'
-
 
 export default {
   name: 'Home',
   components: {
 
     InsertName
-    // Player1,
-    // Player2,
-    // Roll
-
   }
 }
-</script>
