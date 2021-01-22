39a80d9726446059f2f9fc2d7c6ed6ca52e8619c
DZAKYALR <0dzakyalr@gmail.com>
2021-01-22 09:34:21 +0700

    adjust logic dice

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    diceUrl: [
+      'https://i.imgur.com/6DwTTxc.png',
+      'https://i.imgur.com/g1sbMdg.png',
+      'https://i.imgur.com/vvpdsLv.png',
+      'https://i.imgur.com/Gol5vmT.png',
+      'https://i.imgur.com/bAC4F4P.png',
+      'https://i.imgur.com/E3eD8th.png'
+    ]
   },
   mutations: {
   },
